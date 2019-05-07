c6ed878c6118fe3e844186b4dee1787630f72dab
Tawan Yantapan <tawantawan1997@gmail.com>
2019-05-07 23:26:47 +0700

    add routes : path & component , Insert : imports & @NgModule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,20 +7,31 @@ import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { Content2Component } from './content2/content2.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Content3Component } from './content3/content3.component';
 
+const routes : Routes =[
+  {path: 'content', component: ContentComponent},
+  {path: 'content2', component: Content2Component},
+  {path: 'content3', component: Content3Component}, 
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    ContentComponent
+    ContentComponent,
+    Content2Component,
+    Content3Component,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
